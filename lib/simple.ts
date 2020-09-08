6a11b10c336f9cac5388e791ea65bdf82b5e0ff1
Xiaocong Dong <15652914852@163.com>
2020-09-08 08:39:39 +0800

    fix: fix global config conflict

--- lib/simple.ts
+++ lib/simple.ts
@@ -1,26 +1,46 @@
 import parser from './parser'
 import Environment from './runtime/Environment'
 import tokenizer from './Tokenizer'
+import { IToken } from './lexer/types/token'
+import Node from './ast/node/Node'
 
-export default (code: string, config?: any) => {
+export default (code: string, config?: any): {tokens: Array<IToken>, node: Node, tokenizerTime: number, astTime: number, runTime: number} => {
   const env = new Environment()
   env.create('console', {
     log: console.log
   })
   env.create('process', {
   })
+
   if (config && config.global) {
     Object.entries(config.global).forEach(([key, value]) => {
-      env.create(key, value)
+      if (env.get(key) !== undefined) {
+        env.update(key, value)
+      } else {
+        env.create(key, value)
+      }
     })
   }
-  console.time('tokenizer')
+
+  const tokenizerStart = Date.now()
   tokenizer.parse(code)
-  console.timeLog('tokenizer')
-  console.time('parser')
-  const node = parser(tokenizer.getTokenBuffer())
-  console.timeLog('parser')
-  console.time('evaluate')
+  const tokenizerEnd = Date.now()
+
+  const tokens = tokenizer.getTokenBuffer()
+
+  const astStart = Date.now()
+  const node = parser(tokens)
+  const astEnd = Date.now()
+
+  const runStart = Date.now()
   node.evaluate(env)
-  console.timeLog('evaluate')
+  const runEnd = Date.now()
+
+  return {
+    tokens: tokens.toJSON(),
+    node,
+    tokenizerTime: tokenizerEnd - tokenizerStart,
+    astTime: astEnd - astStart,
+    runTime: runEnd - runStart
+  }
 }
